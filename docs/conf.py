68d5a1de8933a98c603f84109976d3d0343d0ff3
Irina <44400304+iribelousova@users.noreply.github.com>
2024-07-05 20:43:17 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,6 @@ extensions = [
 
 # -- Intersphinx options
 intersphinx_mapping = {
-     "euromod": ("https://euromod.readthedocs.io/en/latest/", None),
      "python": ("https://docs.python.org/3/", None),
      "numpy": ("https://numpy.org/doc/stable/", None),
      "pandas": ("https://pandas.pydata.org/docs/", None),
