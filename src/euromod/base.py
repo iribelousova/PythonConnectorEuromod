25bbc89c748420c125693104f7252b0be0a06c5c
belouir <Irina.BELOUSOVA@ec.europa.eu>
2024-07-09 13:58:05 +0200

    docs::adding lincensing

--- src/euromod/base.py
+++ src/euromod/base.py
@@ -1,9 +1,31 @@
-# -*- coding: utf-8 -*-
 """
-Created on Mon Jun  3 11:57:39 2024
 
-@author: serruha
+Copyright 2024 European Commission
+
+*
+
+
+Licensed under the EUPL, Version 1.2;
+
+You may not use this work except in compliance with the Licence.
+
+You may obtain a copy of the Licence at:
+
+*
+
+    https://joinup.ec.europa.eu/software/page/eupl
+
+*
+
+
+Unless required by applicable law or agreed to in writing, software distributed under the Licence is distributed on an "AS IS" basis,
+
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+
+See the Licence for the specific language governing permissions and limitations under the Licence.
+
 """
+
 import os
 import clr as clr
 from utils._paths import  DLL_PATH
