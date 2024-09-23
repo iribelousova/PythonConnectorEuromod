b4ed208dc6088beca138a975cff0ee56abea8cee
Irina <44400304+iribelousova@users.noreply.github.com>
2024-09-23 11:42:54 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
     "sphinx.ext.intersphinx",
-	"sphinxcontrib.jquery"
+	#"sphinxcontrib.jquery"
 ]
 
 # -- Intersphinx options
